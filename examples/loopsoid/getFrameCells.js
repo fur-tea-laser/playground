1afb5d926bb36bf593aeab8bfd22b09ec55d2b92
jared <fur.tea.laser@gmail.com>
2026-03-14 15:46:18 +0000

    feature(loopsoid): closer

--- examples/loopsoid/getFrameCells.js
+++ examples/loopsoid/getFrameCells.js
@@ -1,26 +1,18 @@
 import { loopPoint, loopCosine, loopSine } from "../library/loop/index.js"
-import { spacer, spacerSymmetricSlotWeights } from "../library/spacer/index.js"
+import { phasedSpacer, spacer, spacerSymmetricSlotWeights } from "../library/spacer/index.js"
 
 function getFrameCells(frameCount, frameIndex) {
   const frameStamp = frameIndex / frameCount
-  const azimuthResolution = 3
-  const polarResolution = 512
+  const azimuthResolution = frameCount
+  const azimuthDensity = 5
+  const polarResolution = 1024
   const ringResolution = 31
-  const orientationSpacer = spacer([frameCount, [7,0]])
-  let ringOrientation
-  for (let i=0; i<orientationSpacer[1].length; i++) {
-    if (frameIndex === orientationSpacer[1][i]) {
-      ringOrientation = i
-      break
-    }
-    else if (frameIndex < orientationSpacer[1][i]) {
-      ringOrientation = i - 1
-      break
-    }
-  }
-  const ringSpacer = spacer([ringResolution, [29,0],[19,0],[17,0],[13,0],[11,0],[7,ringOrientation]])
+  const ringOrientation_aa = spacerOrientation(spacer([frameCount, [4,0]]), frameIndex)
+  const ringOrientation_bb = spacerOrientation(spacer([frameCount, [7,0]]), frameIndex)
+  const ringSpacer = spacer([ringResolution, [29,0],[19,0],[17,0],[13,0],[11,0],[7,ringOrientation_bb],[4,ringOrientation_aa]])
   const ringRadiusWeights = spacerSymmetricSlotWeights(spacer([ringResolution, [29,0],[19,0],[17,0]]))
-  const cellCount = azimuthResolution * polarResolution * ringSpacer[1].length * 2
+  const orbShapeWeights = spacerSymmetricSlotWeights(spacer([ringResolution, [29,0],[19,0]]))
+  const cellCount = azimuthDensity * polarResolution * ringSpacer[1].length * 2
   const cellBuffer = Host.getFrameCellBuffer(cellCount)
   const cellView = new DataView(cellBuffer)
   let cellIndex = 0
@@ -40,22 +32,26 @@ function getFrameCells(frameCount, frameIndex) {
   const c = Math.cos(rotationAngle)
   const s = Math.sin(rotationAngle)
   const t = 1 - Math.cos(rotationAngle)
+  const azimuthOrientation_bb = spacerOrientation(spacer([frameCount, [7,0]]), frameIndex)
   for (const ringIndex of ringSpacer[1]) {
-    const orbRadius = ringRadiusWeights[ringIndex] / ringRadiusWeights[0]
+    const azimuthOrientation = spacerOrientation(spacer([ringResolution, [azimuthDensity,0]]), ringIndex)
+    const azimuthSpacer = phasedSpacer(spacer([frameCount, [521,0],[30,0],[29,0],[19,0],[17,0],[13,0],[11,0],[7,azimuthOrientation_bb],[azimuthDensity,azimuthOrientation]]), frameIndex)
+    const ringRadius = ringRadiusWeights[ringIndex] / ringRadiusWeights[0]
+    const orbShape = orbShapeWeights[ringIndex] / orbShapeWeights[0]
     originAngle = ringIndex * ringAngleStep
-    originX = orbRadius * Math.cos(originAngle - Math.PI / 2)
-    originY = orbRadius * Math.sin(originAngle - Math.PI / 2)
+    originX = ringRadius * Math.cos(originAngle - Math.PI / 2)
+    originY = ringRadius * Math.sin(originAngle - Math.PI / 2)
     uX = Math.cos(originAngle)
     uY = Math.sin(originAngle)
-    for (let i=0; i<azimuthResolution; i++) {
+    for (const i of azimuthSpacer[1]) {
       azimuthAngleBase = i * azimuthAngleStep
       for (let j=0; j<polarResolution; j++) {
         polarAngle = j * polarAngleStep
-        azimuthAngle = azimuthAngleBase //+ azimuthAngleStep * Math.sin((i + 1) * 220 * polarAngle + frameStamp)
-        azimuthPoint = loopPoint([[orbRadius,0.75,0,Math.PI/2,0]], azimuthAngle + Math.PI / 2)
+        azimuthAngle = azimuthAngleBase + ringRadiusWeights[ringIndex] * azimuthAngleStep * Math.sin((i + 1) * 220 * polarAngle + frameStamp)
+        azimuthPoint = loopPoint([[orbShape,1,originAngle,Math.PI/2,0]], azimuthAngle + Math.PI / 2)
         azimuthCosine = loopCosine(azimuthPoint)
         azimuthSine = loopSine(azimuthPoint)
-        polarPoint = loopPoint([[orbRadius,0.75,0,Math.PI/2,0]], polarAngle)
+        polarPoint = loopPoint([[orbShape,1,originAngle,Math.PI/2,0]], polarAngle)
         polarCosine = loopCosine(polarPoint)
         polarSine = loopSine(polarPoint)
         baseX = 1 * azimuthSine * polarCosine
@@ -73,7 +69,7 @@ function getFrameCells(frameCount, frameIndex) {
           rotateX + originX,
           rotateY + originY,
           rotateZ + originZ,
-          0.01,
+          0.005,
           255,
           255,
           255
@@ -85,7 +81,7 @@ function getFrameCells(frameCount, frameIndex) {
           -rotateX - originX,
           rotateY + originY,
           rotateZ + originZ,
-          0.01,
+          0.005,
           255,
           255,
           255
@@ -108,4 +104,15 @@ function setFrameCell(cellView, cellIndex, centerX, centerY, centerZ, halfRoot, 
   cellView.setUint8(cellByteOffset + 32, cellRed);
   cellView.setUint8(cellByteOffset + 33, cellGreen);
   cellView.setUint8(cellByteOffset + 34, cellBlue);
+}
+
+function spacerOrientation(someSpacer, targetIndex) {
+  for (let i=0; i<someSpacer[1].length; i++) {
+    if (targetIndex === someSpacer[1][i]) {
+      return i
+    }
+    else if (targetIndex < someSpacer[1][i]) {
+      return i - 1
+    }
+  }
 }
--- scripts/renderAnimation_3d.sh
+++ scripts/renderAnimation_3d.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-deno run --allow-run --allow-write ./programs/renderAnimation_3d.ts "/home/coder/project/examples/loopsoid/getFrameCells.js" "/home/coder/project/examples/loopsoid/output/" "loopsoid" 4096 1.047 512
+deno run --allow-run --allow-write ./programs/renderAnimation_3d.ts "/home/coder/project/examples/loopsoid/getFrameCells.js" "/home/coder/project/examples/loopsoid/output/" "loopsoid" 4096 1.047 522
