4b7e93434e7c287dd1d1328e9ad808268046b801
jared <fur.tea.laser@gmail.com>
2026-03-14 22:23:41 +0000

    feature(loopsoid): stuff

--- scripts/renderAnimation_3d.sh
+++ scripts/renderAnimation_3d.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-deno run --allow-run --allow-write ./programs/renderAnimation_3d.ts "/home/coder/project/examples/loopsoid/getFrameCells.js" "/home/coder/project/examples/loopsoid/output/" "loopsoid" 4096 1.047 522
+deno run --allow-run --allow-write ./programs/renderAnimation_3d.ts "/home/coder/project/examples/loopsoid/getFrameCells.js" "/home/coder/project/examples/loopsoid/output/" "loopsoid" 8192 1.047 522
